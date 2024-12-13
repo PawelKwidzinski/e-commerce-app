b8cf26d769a1fd58a100a53d6fc1d2cb3e5891f7
PawelKwidzinski <solvap83@gmail.com>
2024-12-13 12:04:57 +0100

    Add CRUD and handling exceptions for customer service. Ref. #5

---
+++ services/customer/src/main/java/pl/kwidz/ecommerce/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package pl.kwidz.ecommerce.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+
+    private final String msg;
+}
