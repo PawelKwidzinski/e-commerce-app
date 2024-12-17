e9ad1a6a2fa464a5705c96383af2bbf9dc23ab61
PawelKwidzinski <solvap83@gmail.com>
2024-12-17 11:56:49 +0100

    (feat) CRUD for product. Ref. #7

---
+++ services/product/src/main/java/pl/kwidz/ecommerce/category/Category.java
@@ -0,0 +1,33 @@
+package pl.kwidz.ecommerce.category;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.kwidz.ecommerce.product.Product;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
